078acfea7e44ecf1596b0b2a9be61c31ea0756a6
Vaibhav Singhal <vsinghal85@gmail.com>
2017-11-02 04:02:35 -0400

    Add stocks and ajax for UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def my_portfolio
+  	#@stock= StockQuote.price_by_symbol('AAPL')
+  end
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
+
+
 class WelcomeController < ApplicationController
   def index
-  	
+  	#@stock= StockQuote.price_by_symbol('AAPL')
   end
 end
